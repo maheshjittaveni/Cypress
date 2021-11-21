85654cf18c375d7937f37a9faece281d8313a596
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-11-21 17:27:42 +0530

    first cypress commit

---
+++ cypress/integration/CypressTesting/my_first_test.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("My First Test Suite", function(){
+
+it("This is first cypress test", ()=>{
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise")
+    cy.get('.search-keyword').type("ca")
+    cy.wait(2000)
+    cy.get('.product').should('have.length', 5)
+    cy.get('.product:visible').should('have.length', 4)
+    //parent child chaining
+    cy.get('.products').find('.product').should('have.length', 4)
+    cy.get('.products').find('.product').eq(2).contains("ADD TO CART").click()
+    //cy.get("ADD TO CART")
+    cy.get('.products').find('.product').each(($el,index,$list)=>{
+        const txtVeg=$el.find('h4.priduct-name').text()
+    })
+    
+})
+
+    
+})
